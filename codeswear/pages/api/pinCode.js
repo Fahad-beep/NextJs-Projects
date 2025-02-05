0cff4a79170e7c54586fc5f4d47b251372a47913
Fahad-beep <mfahadadil2004@gmail.com>
2025-02-06 02:22:19 +0500

    updated products page, added pinCode and Buy now option in products page

---
+++ codeswear/pages/api/pinCode.js
@@ -0,0 +1,3 @@
+export default async function pinCode(req, res) {
+  res.status(200).json([1111, 2222, 3333, 4444, 5555]);
+}
--- codeswear/pages/product/[slug].js
+++ codeswear/pages/product/[slug].js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 const Post = () => {
   const router = useRouter();
   const { slug } = router.query;
+  const [pin, setpin] = useState();
+  const [service, setservice] = useState();
+  const checkServiceAval = async () => {
+    const pins = await fetch("http://localhost:3000/api/pinCode");
+    const allPins = await pins.json();
+    console.log("pins: ", pins, allPins);
+    if (allPins.includes(parseInt(pin))) {
+      setservice(true);
+    } else {
+      setservice(false);
+    }
+  };
+  const onChangePin = (e) => {
+    setpin(e.target.value);
+  };
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
@@ -162,7 +177,10 @@ const Post = () => {
                 <span className="title-font font-medium text-2xl text-gray-900">
                   $58.00
                 </span>
-                <button className="flex ml-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
+                <button className="flex ml-4 md:ml-16 text-white bg-pink-500 border-0 py-2 px-2  md:px-6 focus:outline-none hover:bg-pink-600 rounded">
+                  Buy Now
+                </button>
+                <button className="flex ml-2 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">
                   Add To Cart
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
@@ -178,6 +196,30 @@ const Post = () => {
                   </svg>
                 </button>
               </div>
+              <div className="flex space-x-1 mt-6 text-sm">
+                <input
+                  onChange={onChangePin}
+                  type="text"
+                  className="pin border-2 mr-2 border-grey-500 rounded-md px-2 focus:border-red-700"
+                  placeholder="Enter your Pin"
+                />
+                <button
+                  onClick={checkServiceAval}
+                  className="flex ml-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
+                >
+                  Check
+                </button>
+              </div>
+              {!service && service != null && (
+                <div className="wrongPin mt-2 text-red-500">
+                  Sorry! We do not support this pin yet
+                </div>
+              )}
+              {service && service != null && (
+                <div className="correctPin mt-2 text-green-600">
+                  Yay! this pin is code is servicable
+                </div>
+              )}
             </div>
           </div>
         </div>
